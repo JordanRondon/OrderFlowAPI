5ddaf060d68f75a3163503cdadd3e42956624322
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-03-21 16:35:09 -0500

    first commit

---
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/OrderFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.OrderFlowAPI.OrderFlowAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFlowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFlowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OrderFlowAPI/OrderFlowAPI/OrderFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrderFlowAPI.OrderFlowAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
