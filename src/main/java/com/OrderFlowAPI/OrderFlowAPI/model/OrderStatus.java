270edb5ac07a233396975ed15a6b9b9d6c5a9b1e
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-03-24 09:40:28 -0500

    feat(OrderStatus): creation of the OrderStatus class

---
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/OrderStatus.java
@@ -0,0 +1,30 @@
+package com.OrderFlowAPI.OrderFlowAPI.model;
+
+public class OrderStatus {
+    private int statusId;
+    private String name;
+
+    public OrderStatus() {
+    }
+
+    public OrderStatus(int statusId, String name) {
+        this.statusId = statusId;
+        this.name = name;
+    }
+
+    public int getStatusId() {
+        return this.statusId;
+    }
+
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
