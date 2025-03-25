717a1a1c7ffa28f0f1bd9ca6f09a296b4457e481
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-03-25 11:00:10 -0500

    feature(User): mapping the User entity to the database

--- src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/User.java
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/User.java
@@ -1,11 +1,36 @@
 package com.OrderFlowAPI.OrderFlowAPI.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
+
+    @Column(name = "first_name", nullable = false, length = 30)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 30)
     private String lastName;
+
+    @Column(name = "email", unique = true, nullable = false, length = 50)
     private String email;
+
+    @Column(name = "user_password", nullable = false, length = 225)
     private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
     private Role role;
 
     public User() {
