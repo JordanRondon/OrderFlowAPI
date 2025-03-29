0630c41814cf487b26ace655d50a58663802f595
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-03-29 11:50:36 -0500

    feature(ComboProductId): class for composite primary keys

---
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/ComboProductId.java
@@ -0,0 +1,33 @@
+package com.OrderFlowAPI.OrderFlowAPI.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class ComboProductId {
+    private int comboId;
+    private int productId;
+
+    public ComboProductId() {
+    }
+
+    public ComboProductId(int comboId, int productId) {
+        this.comboId = comboId;
+        this.productId = productId;
+    }
+
+    public int getComboId() {
+        return this.comboId;
+    }
+
+    public void setComboId(int comboId) {
+        this.comboId = comboId;
+    }
+
+    public int getProductId() {
+        return this.productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+}
