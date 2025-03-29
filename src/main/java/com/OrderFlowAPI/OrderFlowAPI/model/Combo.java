6cbe001d100af909d4b1764fd508fd7e04d8774a
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-03-29 11:54:51 -0500

    feature(Combo): Combo entity mapping

--- src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/Combo.java
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/Combo.java
@@ -2,12 +2,39 @@ package com.OrderFlowAPI.OrderFlowAPI.model;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "combo")
 public class Combo {
+    @Id
+    @Column(name = "combo_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int comboId;
-    private ProductStatus status;
+
+    @Column(name = "combo_name", nullable = false, length = 30)
     private String name;
+
+    @Column(name = "price", nullable = false)
     private double price;
+
+    @Column(name = "number_of_requests", nullable = false)
     private int numberOfRequests;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id")
+    private ProductStatus status;
+
+    @OneToMany(mappedBy = "combo", cascade = CascadeType.ALL)
     private List<ComboProduct> products;
 
     public Combo() {
