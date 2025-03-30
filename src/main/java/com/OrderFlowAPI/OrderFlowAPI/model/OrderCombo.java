d554bc9ed40ef3fcf2d9d8ed32bac9c29cbe413e
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-03-30 10:00:56 -0500

    fix(OrderCombo): aggregation of notations to map the entity name

--- src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/OrderCombo.java
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/OrderCombo.java
@@ -2,10 +2,14 @@ package com.OrderFlowAPI.OrderFlowAPI.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "order_combo")
 public class OrderCombo {
     @EmbeddedId
     private OrderComboId id;
