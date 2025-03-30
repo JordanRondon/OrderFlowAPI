d6435b3a8ce4a228366a6e48c825d46a2b9903c4
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-03-30 09:25:18 -0500

    feature(OrderProduct): OrderProduct entity mapping

--- src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/OrderPorduct.java
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/OrderPorduct.java
@@ -1,7 +1,26 @@
 package com.OrderFlowAPI.OrderFlowAPI.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+
 public class OrderPorduct {
+    @EmbeddedId
+    private OrderPorductId id;
+
+    @ManyToOne
+    @MapsId("orderId")
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @MapsId("productId")
+    @JoinColumn(name = "product_id")
     private Product product;
+
+    @Column(name = "requested_amount")
     private int requestedAmount;
 
     public OrderPorduct() {
