b8a04e8e278992b2a41f942fcd0a3357222cc22e
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-04-16 16:15:39 -0500

    refactor(model): add abstract class for product

---
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/model/AbstractProduct.java
@@ -0,0 +1,35 @@
+package com.OrderFlowAPI.OrderFlowAPI.model;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class AbstractProduct {
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    @Column(name = "number_of_requests", nullable = false)
+    private int numberOfRequests = 0;
+
+    @ManyToAny
+    @JoinColumn(name = "status_id")
+    private ProductStatus status;
+
+    public AbstractProduct(String name, double price, int numberOfRequests) {
+        this.name = name;
+        this.price = price;
+        this.numberOfRequests = numberOfRequests;
+    }
+}
