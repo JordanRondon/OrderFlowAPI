329970bdb8d1f099eb2d2be36f810fbe3fb671fc
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-04-16 19:23:32 -0500

    feat(repository, service, controller): endpoint development to obtain a user by ID

---
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/repository/IUserRepository.java
@@ -0,0 +1,11 @@
+package com.OrderFlowAPI.OrderFlowAPI.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.OrderFlowAPI.OrderFlowAPI.model.User;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Integer> {
+
+}
