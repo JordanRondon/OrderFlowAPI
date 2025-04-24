2af33bfa9c459ab5a6151d3f43baf0fc0d00863a
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-04-24 18:13:33 -0500

    feat(dto, mapper): Integration of classes for DTO and mapping

---
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.OrderFlowAPI.OrderFlowAPI.mapper;
+
+import com.OrderFlowAPI.OrderFlowAPI.dto.UserDto;
+import com.OrderFlowAPI.OrderFlowAPI.model.User;
+
+public class UserMapper {
+    public static UserDto toDto(User user) {
+        UserDto dto = new UserDto();
+        dto.setUserId(user.getUserId());
+        dto.setFirstName(user.getFirstName());
+        dto.setLastName(user.getLastName());
+        dto.setEmail(user.getEmail());
+        dto.setRole(user.getRole());
+        return dto;
+    }
+
+    public static User toEntity(UserDto userDto) {
+        User user = new User();
+        user.setUserId(userDto.getUserId());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        user.setRole(userDto.getRole());
+        return user;
+    }
+}
