068167813e4a94583316cdd594969727436b5759
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-05-13 11:48:48 -0500

    chore(IUserService): packet change in request and response

--- src/main/java/com/OrderFlowAPI/OrderFlowAPI/service/IUserService.java
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/service/IUserService.java
@@ -1,9 +1,9 @@
 package com.OrderFlowAPI.OrderFlowAPI.service;
 
-import com.OrderFlowAPI.OrderFlowAPI.dto.LoginRequestDto;
-import com.OrderFlowAPI.OrderFlowAPI.dto.LoginResponseDto;
-import com.OrderFlowAPI.OrderFlowAPI.dto.RegisterRequestDto;
 import com.OrderFlowAPI.OrderFlowAPI.dto.UserDto;
+import com.OrderFlowAPI.OrderFlowAPI.dto.request.LoginRequestDto;
+import com.OrderFlowAPI.OrderFlowAPI.dto.request.RegisterRequestDto;
+import com.OrderFlowAPI.OrderFlowAPI.dto.response.LoginResponseDto;
 
 public interface IUserService {
     public void register(RegisterRequestDto registerRequestDto);
