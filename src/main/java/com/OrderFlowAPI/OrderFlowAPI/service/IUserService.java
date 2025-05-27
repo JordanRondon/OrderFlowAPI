5bc5d226092924909c765e1edf820e33d52dd40c
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-05-27 11:42:17 -0500

    refactor(service): separation of user logic with auth

--- src/main/java/com/OrderFlowAPI/OrderFlowAPI/service/IUserService.java
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/service/IUserService.java
@@ -1,14 +1,7 @@
 package com.OrderFlowAPI.OrderFlowAPI.service;
 
 import com.OrderFlowAPI.OrderFlowAPI.dto.UserDto;
-import com.OrderFlowAPI.OrderFlowAPI.dto.request.LoginRequestDto;
-import com.OrderFlowAPI.OrderFlowAPI.dto.request.RegisterRequestDto;
-import com.OrderFlowAPI.OrderFlowAPI.dto.response.LoginResponseDto;
 
 public interface IUserService {
-    public void register(RegisterRequestDto registerRequestDto);
-
-    public LoginResponseDto login(LoginRequestDto loginRequestDto);
-
     public UserDto getUserById(int userId);
 }
