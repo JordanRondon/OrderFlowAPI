9171a10e1849ecc3efdd4b8036d7ab35d143a954
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-05-29 18:20:02 -0500

    feat(user): Added DTO user status and in the mapper

--- src/main/java/com/OrderFlowAPI/OrderFlowAPI/dto/UserDto.java
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/dto/UserDto.java
@@ -17,4 +17,5 @@ public class UserDto {
     private String lastName;
     private String email;
     private RoleDto roleDto;
+    private StatusDto statusDto;
 }
--- src/main/java/com/OrderFlowAPI/OrderFlowAPI/mapper/UserMapper.java
+++ src/main/java/com/OrderFlowAPI/OrderFlowAPI/mapper/UserMapper.java
@@ -1,30 +1,36 @@
 package com.OrderFlowAPI.OrderFlowAPI.mapper;
 
 import com.OrderFlowAPI.OrderFlowAPI.dto.RoleDto;
+import com.OrderFlowAPI.OrderFlowAPI.dto.StatusDto;
 import com.OrderFlowAPI.OrderFlowAPI.dto.UserDto;
 import com.OrderFlowAPI.OrderFlowAPI.model.Role;
 import com.OrderFlowAPI.OrderFlowAPI.model.User;
+import com.OrderFlowAPI.OrderFlowAPI.model.UserStatus;
 
 public class UserMapper {
     public static UserDto toDto(User user) {
         RoleDto roleDto = RoleMapper.toDto(user.getRole());
+        StatusDto statusDto = StatusMapper.toDto(user.getStatus());
         return UserDto.builder()
                 .userId(user.getUserId())
                 .firstName(user.getFirstName())
                 .lastName(user.getLastName())
                 .email(user.getEmail())
                 .roleDto(roleDto)
+                .statusDto(statusDto)
                 .build();
     }
 
     public static User toEntity(UserDto userDto) {
+        Role role = RoleMapper.toEntity(userDto.getRoleDto());
+        UserStatus userStatus = StatusMapper.toEntityUserStatus(userDto.getStatusDto());
         User user = new User();
         user.setUserId(userDto.getUserId());
         user.setFirstName(userDto.getFirstName());
         user.setLastName(userDto.getLastName());
         user.setEmail(userDto.getEmail());
-        Role role = RoleMapper.toEntity(userDto.getRoleDto());
         user.setRole(role);
+        user.setStatus(userStatus);
         return user;
     }
 }
